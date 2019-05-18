5086303b1b1c0d18109cb78c9281414de20039e8
Mark Wissink <markcwissink@gmail.com>
2019-05-18 15:10:41 -0400

    Added editorconfig

--- init.el
+++ init.el
@@ -32,4 +32,17 @@
 
 ;; Other customization
 (tool-bar-mode -1)
+(menu-bar-mode -1)
 (setq initial-scratch-message nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (editorconfig use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

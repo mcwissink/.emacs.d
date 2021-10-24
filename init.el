7e210d7ca2119bf69b70c33c0b28193551131a55
Mark Wissink <markcwissink@gmail.com>
2021-10-24 00:21:20 -0400

    init

---
+++ init.el
@@ -0,0 +1,188 @@
+;;; init.el --- Mark Wissink
+
+;;; Commentary:
+
+;;; Code:
+
+;;; random fixes
+;; Fix for https://debbugs.gnu.org/cgi/bugreport.cgi?bug=34341
+;;(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+;; (setq ghub-use-workaround-for-emacs-bug 'force)
+
+;;; bootstrap custom file
+(let ((custom-file-path "~/.emacs.d/custom.el"))
+  (unless (file-exists-p custom-file-path)
+        (with-temp-buffer (write-file custom-file-path)))
+  (setq custom-file custom-file-path)
+  (load custom-file))
+
+;;; bootstrap device file
+;; useful for any device specific configurations
+(let ((device-file-path "~/.emacs.d/device.el"))
+  (unless (file-exists-p device-file-path)
+        (with-temp-buffer (write-file device-file-path)))
+  (load device-file-path))
+
+;;; bootstrap use-package
+(require 'package)
+(package-initialize)
+
+(setq package-archives
+  '(("gnu" . "https://elpa.gnu.org/packages/")
+     ("melpa" . "https://melpa.org/packages/")))
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package)
+  (setq use-package-always-ensure t))
+
+(use-package use-package-ensure-system-package)
+
+;;; coding 
+;; lsp
+(use-package lsp-mode
+  :commands
+  lsp
+  :hook
+  (
+    (typescript-mode . lsp)
+    (web-mode . lsp)
+    (js-mode . lsp))
+  :custom
+  (lsp-headerline-breadcrumb-enable nil))
+
+(use-package lsp-ui
+  :commands
+  lsp-ui-mode)
+
+(use-package lsp-ivy
+  :commands
+  lsp-ivy-workspace-symbol)
+
+(use-package lsp-treemacs
+  :commands
+  lsp-treemacs-errors-list)
+
+;; major modes
+(use-package handlebars-mode
+  :mode
+  "\\.tpl\\'")
+
+(use-package web-mode
+  :custom
+  (web-mode-enable-auto-quoting nil)
+  :mode
+  "\\.tsx\\'")
+
+(use-package lsp-tailwindcss
+  :init
+  (setq lsp-tailwindcss-add-on-mode t)
+  (setq lsp-tailwindcss-auto-install-server nil))
+
+;;; editor
+(use-package vterm)
+
+(use-package editorconfig
+  :config
+  (editorconfig-mode 1))
+
+(use-package evil
+  :config
+  (evil-set-initial-state 'vterm-mode 'emacs)
+  (evil-set-undo-system 'undo-tree)
+  (evil-mode 1)
+  ;; custom evil bindings
+  (define-key evil-normal-state-map (kbd "zr") 'query-replace)
+  (define-key evil-normal-state-map (kbd "zR") 'lsp-rename)
+  (define-key evil-normal-state-map (kbd "zf") 'swiper)
+  (define-key evil-normal-state-map (kbd "zj") 'flycheck-next-error)
+  (define-key evil-normal-state-map (kbd "zk") 'flycheck-previous-error))
+
+
+(use-package undo-tree
+  :config
+  (global-undo-tree-mode))
+
+(use-package ivy
+  :bind
+  (
+    ("C-s" . swiper))
+  :config
+  (ivy-mode 1))
+
+(use-package counsel
+  :demand
+  :bind
+  (
+    ("C-x C-p" . counsel-fzf)
+    ("C-x a" . counsel-rg))
+  :config
+  (counsel-mode 1))
+
+(use-package company
+  :config
+  (global-company-mode 1))
+
+(use-package projectile
+  :bind
+  (
+    ("C-x p" . projectile-find-file))
+  :config
+  (projectile-mode 1))
+
+(use-package magit)
+
+;;; theme
+(use-package apropospriate-theme
+  :config
+  (load-theme 'apropospriate-dark t))
+
+(custom-set-faces
+  '(mode-line ((t (:background "#424242" :box (:line-width 4 :color "#424242")))))
+  '(mode-line-inactive ((t (:background "#424242" :box (:line-width 4 :color "#424242"))))))
+
+;; remove scratch message
+(setq initial-scratch-message nil)
+;; disable the awful bell sound
+(setq ring-bell-function 'ignore)
+;; reoad files when modified by external program
+(global-auto-revert-mode 1)
+;; disable backups
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+(setq create-lockfiles nil)
+
+;; remove unnecessary emacs ui
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+
+;; reuse directory buffer
+(put 'dired-find-alternate-file 'disabled nil)
+
+;; simplify y or n
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; remove annoying bindings
+(unbind-key "s-t")
+(unbind-key "s-p")
+
+;; fix large file performance problems
+(setq read-process-output-max (* 1024 1024))
+
+
+
+;;; custom functions
+(defun copy-file-name-to-clipboard ()
+  "Copy the current buffer file name to the clipboard."
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode) default-directory (buffer-file-name))))
+    (when filename
+      (kill-new filename)
+      (message "Copied buffer file name '%s' to the clipboard." filename))))
+
+(provide 'init)
+;;; init.el ends here

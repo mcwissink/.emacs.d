b13578709097227207c674a3cb08be5068bba1c8
Mark Wissink <mark.wissink@tekton.com>
2024-04-30 07:36:03 -0400

    new and improved

--- init.el
+++ init.el
@@ -1,13 +1,5 @@
-;;; init.el --- Mark Wissink
-
-;;; Commentary:
-
-;;; Code:
-
-;;; random fixes
-;; Fix for https://debbugs.gnu.org/cgi/bugreport.cgi?bug=34341
-;; (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
-;; (setq ghub-use-workaround-for-emacs-bug 'force)
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
 
 ;; remove bindings
 (global-unset-key (kbd "s-t"))
@@ -29,42 +21,33 @@
 ;; useful for any device specific configurations
 (ensure-file "~/.emacs.d/device.el" 'load)
 
-;;; bootstrap use-package
-(require 'package)
-(package-initialize)
-
 (setq package-archives
   '(("gnu" . "https://elpa.gnu.org/packages/")
      ("melpa" . "https://melpa.org/packages/")))
 
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(eval-when-compile
-  (require 'use-package)
-  (setq use-package-always-ensure t))
-
-(use-package use-package-ensure-system-package)
-
 ;;; coding
 ;; lsp-mode is required until eglot supports multiple language servers for a single mode
+;; https://github.com/minad/corfu/wiki#basic-example-configuration-with-orderless
 (use-package lsp-mode
   :bind
   ("C-c C-a" . lsp-execute-code-action)
   ("C-c C-r" . lsp-rename)
   :commands
   lsp
+  :init
+  (defun my/lsp-completion-mode ()
+    (setf (alist-get 'styles (alist-get 'lsp-capf completion-category-defaults))
+          '(orderless)))
   :hook
   ((tsx-ts-mode . lsp-deferred)
-   (typescript-ts-mode . lsp-deferred))
+   (typescript-ts-mode . lsp-deferred)
+   (lsp-completion-mode . my/lsp-completion-mode))
   :custom
   (lsp-auto-guess-root t)
+  (lsp-completion-provider :none)
   (lsp-headerline-breadcrumb-enable nil))
 
 (use-package lsp-ui :commands lsp-ui-mode)
-
-(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
 
 (use-package vterm
   :config
@@ -104,34 +87,69 @@
   (evil-define-key 'normal 'global (kbd "gd") 'lsp-find-definition)
   (evil-define-key 'normal 'global (kbd "gr") 'lsp-find-references)
   (evil-define-key 'normal 'global (kbd "K") 'lsp-ui-doc-glance)
-  (evil-define-key 'normal 'global (kbd "E") 'flymake-goto-next-error)
+  (evil-define-key 'normal 'global (kbd "E") 'consult-flymake)
   (evil-define-key 'emacs 'vterm-mode-map (kbd "C-<tab>") 'multi-vterm-next))
 
-(use-package ivy
-  :bind
-  ("C-s" . swiper)
-  ("C-<return>" . ivy-immediate-done)
-  :config
-  (ivy-mode 1))
+(use-package orderless
+  :custom
+  (completion-styles '(orderless basic)))
 
-(use-package ivy-xref
+(use-package marginalia
   :ensure t
-  :init
-  (setq xref-show-definitions-function #'ivy-xref-show-defs)
-  (setq xref-show-xrefs-function #'ivy-xref-show-xrefs))
-
-(use-package counsel
-  :demand
-  :bind
-  ("C-c C-g" . counsel-rg)
   :config
-  (counsel-mode))
+  (marginalia-mode))
+
+(use-package embark
+  :bind
+  ("C-." . embark-act))
+
+(use-package embark-consult)
+
+(use-package vertico
+  :init
+  (vertico-mode)
+  :custom
+  (vertico-sort-function 'vertico-sort-alpha))
+
+(use-package consult
+  :bind
+  ("C-c C-g" . consult-ripgrep)
+  ("C-x b" . consult-buffer)
+  ("C-s" . consult-line)
+  :init
+  (setq xref-show-xrefs-function #'consult-xref)
+  (setq xref-show-definitions-function #'consult-xref))
 
 (use-package wgrep)
 
 (use-package corfu
+  :custom
+  (corfu-auto t)
+  (corfu-cycle t)
   :config
   (global-corfu-mode))
+
+(use-package company)
+
+(use-package cape
+  :init
+  ;; Add to the global default value of `completion-at-point-functions' which is
+  ;; used by `completion-at-point'.  The order of the functions matters, the
+  ;; first function returning a result wins.  Note that the list of buffer-local
+  ;; completion functions takes precedence over the global list.
+  (add-to-list 'completion-at-point-functions #'cape-dabbrev)
+  (add-to-list 'completion-at-point-functions #'cape-file)
+  (add-to-list 'completion-at-point-functions #'cape-elisp-block)
+  ;;(add-to-list 'completion-at-point-functions #'cape-history)
+  ;;(add-to-list 'completion-at-point-functions #'cape-keyword)
+  ;;(add-to-list 'completion-at-point-functions #'cape-tex)
+  ;;(add-to-list 'completion-at-point-functions #'cape-sgml)
+  ;;(add-to-list 'completion-at-point-functions #'cape-rfc1345)
+  ;;(add-to-list 'completion-at-point-functions #'cape-abbrev)
+  ;;(add-to-list 'completion-at-point-functions #'cape-dict)
+  ;;(add-to-list 'completion-at-point-functions #'cape-elisp-symbol)
+  ;;(add-to-list 'completion-at-point-functions #'cape-line)
+)
 
 (use-package projectile
   :bind
